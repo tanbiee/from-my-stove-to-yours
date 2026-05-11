85d006c061dd9bfef13daf8ec83ac1586fad3150
Deepti <pathaniadeepti05@gmail.com>
2026-05-11 18:49:43 +0530

    Switch to MongoDB Atlas and clean up collections

--- config/database.php
+++ config/database.php
@@ -114,6 +114,12 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('DB_URI', 'mongodb://localhost:27017'),
+            'database' => env('DB_DATABASE', 'laravel'),
+        ],
+
     ],
 
     /*
