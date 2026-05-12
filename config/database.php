f341dfa8d6919400bf69de8f74497149124f9189
Deepti <pathaniadeepti05@gmail.com>
2026-05-12 13:27:37 +0530

    Switch to new MongoDB Atlas cluster and fix duplicate database config

--- config/database.php
+++ config/database.php
@@ -120,12 +120,6 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
-        'mongodb' => [
-            'driver' => 'mongodb',
-            'dsn' => env('DB_URI', 'mongodb://localhost:27017'),
-            'database' => env('DB_DATABASE', 'laravel'),
-        ],
-
     ],
 
     /*
